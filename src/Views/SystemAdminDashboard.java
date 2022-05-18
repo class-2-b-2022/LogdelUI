85500a53ecaca15e0c52ac5e42c56dfaf12d67a1
IsiteYves <yvesisite@gmail.com>
2022-05-18 09:33:13 +0200

    [feat] Resolved java.desktop erros in module-info.java

---
+++ src/Views/SystemAdminDashboard.java
@@ -0,0 +1,9 @@
+package Views;
+
+import java.io.IOException;
+
+public class SystemAdminDashboard {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Now running!");
+    }
+}
