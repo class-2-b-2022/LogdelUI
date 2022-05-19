d1c9c5923945dc8b7c94293f7f9a183c11c9820d
IsiteYves <yvesisite@gmail.com>
2022-05-19 15:41:33 +0200

    [FEAT] Worked on the UI of system admin|

--- src/Views/SystemAdminDashboard.java
+++ src/Views/SystemAdminDashboard.java
@@ -1,71 +1,82 @@
 package Views;
 
 import javax.swing.*;
+
 import utils.Theme;
+
 import java.awt.*;
 
-public class SystemAdminDashboard {
-    final static boolean shouldFill = true;
-    final static boolean shouldWeightX = true;
-    final static boolean RIGHT_TO_LEFT = false;
+public class SystemAdminDashboard extends JPanel {
+    GridBagConstraints constraints = new GridBagConstraints();
+    Panel navbar = new Panel();
 
-    public static void addComponentsToPane(Container pane) {
-        pane.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        GridBagConstraints c1 = new GridBagConstraints();
+    public SystemAdminDashboard() {
+        setLayout(new GridBagLayout());
+        constraints.weightx = 1.0;
+        constraints.weighty = 1.0;
+        constraints.fill = GridBagConstraints.BOTH;
+        int x, y;  // for clarity
+        constraints.gridheight = 7; // span two rows
 
-        Panel rightHandSide = new Panel();
         Panel sidebar = new Panel();
+        sidebar.setBackground(new Color(31, 31, 115));
+        addGB(sidebar,   x = 0, y = 0, 0.2);
+        constraints.gridheight = 1; // set it back
 
-        Panel navbar = new Panel();
-        rightHandSide.setLayout(new GridBagLayout());
-        navbar.setBackground(new Color(51, 51, 204));
-        c1.fill = GridBagConstraints.HORIZONTAL;
-        c1.weightx = 1;
-        c1.gridx = 0;
-        c1.ipady = 50;
-        c1.gridy = 0;
-        rightHandSide.add(navbar, c1);
+        Label appTitle = new Label();
+        appTitle.setText(" LODGEL APP");
+        appTitle.setForeground(Color.white);
+        appTitle.setBounds(0, 0, 300, 40);
+        appTitle.setFont(new Font("Arial", Font.BOLD, 18));
 
+        navbar.setBackground(new Color(000033));
+        navbar.setLayout(new GridLayout(1, 2));
+        navbar.add(appTitle);
+        addGB(navbar,   x = 1, y = 0, 0.8);
 
-        rightHandSide.setBackground(new Theme().getPrimary());
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridwidth = 1;
-        c.weightx = 0.8;
-        c.gridx = 1;
-        c.ipady = 768;
-        c.gridy = 0;
-        pane.add(rightHandSide, c);
+        Panel contentHolder = new Panel();
+        contentHolder.setBackground(new Color(0x303057));
+        addGB(contentHolder, x=1, y=1, 0.8);
 
-        sidebar.setBackground(new Color(0, 51, 204));
-        c.weightx = 0.2;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridwidth = 1;
-        c.gridx = 0;
-        c.gridy = 0;
-        c.ipady = 768;
-        pane.add(sidebar, c);
+        Panel contentHolder1 = new Panel();
+        contentHolder1.setBackground(new Color(0x303057));
+        addGB(contentHolder1, x=1, y=2, 0.8);
+
+        Panel contentHolder2 = new Panel();
+        contentHolder2.setBackground(new Color(0x303057));
+        addGB(contentHolder2, x=1, y=3, 0.8);
+
+        Panel contentHolder3 = new Panel();
+        contentHolder3.setBackground(new Color(0x303057));
+        addGB(contentHolder3, x=1, y=4, 0.8);
+
+        Panel contentHolder4 = new Panel();
+        contentHolder4.setBackground(new Color(0x303057));
+        addGB(contentHolder4, x=1, y=5, 0.8);
+
+        Panel contentHolder5 = new Panel();
+        contentHolder5.setBackground(new Color(0x303057));
+        addGB(contentHolder5, x=1, y=6, 0.8);
+
+//        constraints.gridwidth = 2; // span two columns
+//        addGB(new JButton("four"),  x = 1, y = 1, 1.0);
+        constraints.gridwidth = 1; // set it back
+        constraints.gridheight = 1;
     }
 
-
-    private static void createAndShowGUI() {
-//Create and set up the window.
-        JFrame frame = new JFrame("System Admin | Dashboard");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-//Set up the content pane.
-        addComponentsToPane(frame.getContentPane());
-
-//Display the window.
-        frame.setBounds(0, 0, 1366, 768);
-        frame.setVisible(true);
+    void addGB(Component component, int x, int y,double weightx) {
+        constraints.weightx = weightx;
+        constraints.gridx = x;
+        constraints.gridy = y;
+        add(component, constraints);
     }
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
+        JFrame frame = new JFrame("System Admin | Dashboard");
+        frame.setIconImage((new ImageIcon("assets\\logo1.png")).getImage());
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setBounds(0, 0, 1366, 768);
+        frame.setContentPane(new SystemAdminDashboard());
+        frame.setVisible(true);
     }
 }
